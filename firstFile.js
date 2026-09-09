72314bb818e8bf52ede5ae5ef9ec14c49ffd4f7a
Roshini <137187489+NightcoreRagod@users.noreply.github.com>
2026-09-09 16:57:50 +0530

    alert/prompt

---
+++ firstFile.js
@@ -0,0 +1,10 @@
+//comment
+console.log("hello World");
+ let a=4;
+ let b=6;
+
+ console.log("a + b =", a+b);
+
+ alert("be aware");
+let reply= prompt("this message says hello");
+console.log(reply);
