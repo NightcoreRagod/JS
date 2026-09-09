0f78835dce5d029736c5a7ab0c203f1fffdbc805
Roshini <137187489+NightcoreRagod@users.noreply.github.com>
2026-09-09 17:23:55 +0530

    else if statement in js

--- firstFile.js
+++ firstFile.js
@@ -1,10 +1,29 @@
 //comment
-console.log("hello World");
+/*console.log("hello World");
  let a=4;
  let b=6;
 
  console.log("a + b =", a+b);
 
  alert("be aware");
-let reply= prompt("this message says hello");
-console.log(reply);
+let reply= prompt("Check if your number is a multiple of 5");
+if (reply % 5 == 0){
+    console.log('yes');
+}else{
+    console.log('no');
+}
+*/
+let score = prompt("Enter score your here");
+if(90<score && score<100){
+    console.log("A");
+}else if(70<score && score<89){
+    console.log("B");
+}else if(60<score && score<79){
+    console.log("C");
+}else if(50<score && score<59){
+    console.log("D");
+}else if(49>score){
+    console.log("F");
+}else{
+    console.log("Invalid");
+}
