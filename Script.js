d67dcc0a859a7ab3c037b5026e68ebb4a7314d42
Roshini <137187489+NightcoreRagod@users.noreply.github.com>
2026-09-19 22:34:59 +0530

    debugging

--- Script.js
+++ Script.js
@@ -13,59 +13,60 @@ const resultMessage = document.getElementById("result-Message");
 const restartButton = document.getElementById("restart-Btn");
 const progressBar = document.getElementById("progress");
 
-const quizQuestions =[{
-    question: "What is the capital of France?",
-    answer:[
-        {Text:"London", correct: false},
-        {Text:"Berlin", correct: false},
-        {Text:"Paris", correct: true},
-        {Text:"Madrid", correct: false},
-    ],
-
-},{
-    question: "What is the largest ocean on Earth?",
-    answer:[
-        {Text:"Atlantic Ocean", correct: false},
-        {Text:"Indian Ocean", correct: false},
-        {Text:"Arctic Ocean", correct: false},
-        {Text:"Pacific", correct: true},
-    ],
-
-},{
-    question: "What is the chemical of gold?",
-    answer:[
-        {Text:"Gd", correct: false},
-        {Text:"Go", correct: false},
-        {Text:"Au", correct: true},
-        {Text:"Ag", correct: false},
-    ],
-  
-},{
-    question: "Which planet is known as the Red Planet?",
-    answer:[
-        {Text:"Venus", correct: false},
-        {Text:"Mars", correct: true},
-        {Text:"Jupiter", correct: false},
-        {Text:"Saturn", correct: false},
-    ],
-
-},{
+const quizQuestion = [
+    {
+        question: "What is the capital of France?",
+        answer:[
+            {Text:"London", correct: false},
+            {Text:"Berlin", correct: false},
+            {Text:"Paris", correct: true},
+            {Text:"Madrid", correct: false},
+        ]
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        answer:[
+            {Text:"Atlantic Ocean", correct: false},
+            {Text:"Indian Ocean", correct: false},
+            {Text:"Arctic Ocean", correct: false},
+            {Text:"Pacific", correct: true},
+        ],
+    },
+    {
+        question: "What is the chemical of gold?",
+        answer:[
+            {Text:"Gd", correct: false},
+            {Text:"Go", correct: false},
+            {Text:"Au", correct: true},
+            {Text:"Ag", correct: false},
+            ],
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answer:[
+            {Text:"Venus", correct: false},
+            {Text:"Mars", correct: true},
+            {Text:"Jupiter", correct: false},
+            {Text:"Saturn", correct: false},
+            ],
+    },
+    {
     question: "Which of these is not a programming Language?",
     answer:[
         {Text:"Java", correct: false},
         {Text:"python", correct: false},
         {Text:"ox", correct: true},
         {Text:"JavaScript", correct: false},
-    ],
-
-}];
+        ],
+    },
+];
  
 let CurrentQuestionIndex = 0;
 let score = 0;
 let answersDisabled = false
 
-totalQuestionSpan.textContent= quizQuestions.length;
-maxScoreSpan.textContent = quizQuestions.length;
+totalQuestionSpan.textContent= quizQuestion.length;
+maxScoreSpan.textContent = quizQuestion.length;
 
 // event listeners
 
@@ -87,7 +88,7 @@ function showQuestion(){
 
     answersDisabled = false
 
-    const currentQuestion = quizQuestions[CurrentQuestionIndex]
+    const currentQuestion = quizQuestion[CurrentQuestionIndex]
 
     CurrentQuestionSpan.textContent= CurrentQuestionIndex + 1;
 
@@ -95,65 +96,65 @@ function showQuestion(){
 
     progressBar.style.width = progressPercent + "%"
 
-    questionText.textContent = currentQuestion.question
+    questionText.textContent = CurrentQuestionIndex.question;
 
-    answersContainer.innerHTML="";
+    answerContainer.innerHTML="";
 
      currentQuestion.answer.forEach(answer=>{
-        const button = document.createElement("button")
-        button.textContent = answer.text
-        button.classList.add("answer-btn")
+        const button = document.createElement("button");
+        button.textContent = answer.text;
+        button.classList.add("answer-btn");
 
-        button.dataset.correct = answer.correct
+        button.dataset.correct = answer.correct;
 
-        button.addEventListener("click",selectAnswer)
+        button.addEventListener("click",selectAnswer);
 
         answerContainer.appendChild(button);
      });
 }
 
 function selectAnswer(event) {
-    if(answersDisabled) return
+    if(answersDisabled) return;
 
-    answersDisabled = true
+    answersDisabled = true;
 
     const selectedButton = event.target;
     const isCorrect = selectedButton.dataset.correct==="true"
 
     Array.from(answerContainer.children).forEach(button=>{
         if(button.dataset.correct ==='true'){
-            button.classList.add("correct")
+            button.classList.add("correct");
         }else{
-            button.classList.add("incorrect")
+            button.classList.add("incorrect");
         }
     });
 
     if(isCorrect){
         score++;
-        scoreSpan.textContent = score
+        scoreSpan.textContent = score;
     }
 }
 
 setTimeout (()=>{
     CurrentQuestionIndex++;
 
-    if(CurrentQuestionIndex<quizQuestions.length){
-        showQuestion()
+    if(CurrentQuestionIndex < quizQuestions.length){
+        showQuestion();
     }else{
-        showResults()
+        showResults();
     }
-},1000)
+},1000);
 
 function showResults(){
-    quizScreen.classList.remove("active")
-    resultScreen.classList.add("active")
+    quizScreen.classList.remove("active");
+    resultScreen.classList.add("active");
 
     finalScoreSpan.textContent = score;
 
-    const percentage =(score/quizQuestions.length) * 100
+    const percentage =(score/quizQuestions.length) * 100;
 
     if(percentage === 100){
-        resultMessage.textContent = "Perfect! your a genius";
+        resultMessage.textContent = "Perfect! your a genius!";
     }else if(percentage>=80){
         resultMessage.textContent = "Great! you know your stuff";
     }else if(percentage>=60){
